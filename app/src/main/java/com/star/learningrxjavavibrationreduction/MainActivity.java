7f1b50e63d9d114f84bd0255e5db7a1c07e2fa5a
crystalDf <chendong333@gmail.com>
2018-06-28 23:46:06 +0800

    LearningRxJavaVibrationReduction

--- app/src/main/java/com/star/learningrxjavavibrationreduction/MainActivity.java
+++ app/src/main/java/com/star/learningrxjavavibrationreduction/MainActivity.java
@@ -1,13 +1,53 @@
 package com.star.learningrxjavavibrationreduction;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+
+import com.jakewharton.rxbinding2.view.RxView;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "RxJava";
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button);
+
+        RxView
+                .clicks(mButton)
+                .throttleFirst(2, TimeUnit.SECONDS)
+                .subscribe(new Observer<Object>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+
+                    @Override
+                    public void onNext(Object o) {
+                        Log.d(TAG, "发送了网络请求");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应 " + e.toString());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+                });
     }
 }
